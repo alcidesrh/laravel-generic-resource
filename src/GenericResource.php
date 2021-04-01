62d3dfcc79d35d59fbecf9ce9da7d45ae0002bbc
alcidesrh <alcidesrh@gmail.com>
2021-04-01 01:31:18 -0400

    Nested collection property parse bug fixed.

--- src/GenericResource.php
+++ src/GenericResource.php
@@ -2,19 +2,20 @@
 
 namespace Alcidesrh\Generic;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class GenericResource extends JsonResource
 {
     private $fields;
 
-    public function __construct($query, $fields = false)
+    public function __construct($query, $fields = null)
     {
         $this->fields = $fields;
         parent::__construct($query);
     }
 
-    public function setFields($fields)
+    public function setFields(array $fields = null)
     {
         $this->fields = $fields;
         return $this;
@@ -29,12 +30,19 @@ class GenericResource extends JsonResource
     {
 
         if (!$this->fields) {
-            return \get_object_vars($this)['resource'];
-        }
 
-        if (is_array($this->fields)) {
+            $data = \get_object_vars($this)['resource'];
+            $data = $data->toArray();
 
-            $return = [];
+            if (isset($data['pivot'])) {
+                unset($data['pivot']);
+            }
+
+            return $data;
+
+        } else if (is_array($this->fields)) {
+
+            $data = [];
 
             foreach ($this->fields as $key => $value) {
 
@@ -42,33 +50,39 @@ class GenericResource extends JsonResource
 
                     foreach ($value as $key2 => $value2) {
                         try {
-                            if (\gettype($this->{$key2}) === 'string') {
-                                $return[$key2] = $this->{$value2};
-                            } else {
-                                $return[$key2] = new GenericResource($this->{$key2}, $value2);
+                            if ($this->{$key2} instanceof Collection) {
+                                $data[$key2] = new GenericResourceCollection($this->{$key2}, $value2);
+                            } else if (\gettype($this->{$key2}) === 'object') {
+                                $data[$key2] = new GenericResource($this->{$key2}, $value2);
+                            } else if (\gettype($this->{$key2}) === 'string') {
+                                $data[$key2] = $this->{$value2};
                             }
+
                         } catch (\Throwable $th) {
-                           
+
                         }
                     }
                 } else if (\gettype($value) === 'string') {
 
                     try {
-                        $data = $this->{$value};
 
-                        if (\is_numeric($key)) {
-                            $return[$value] = $data;
-                        } else {
-                            $return[$key] = $data;
+                        $newKey = !\is_numeric($key) ? $key : $value;
+
+                        if ($this->{$value} instanceof Collection) {
+                            $data[$newKey] = new GenericResourceCollection($this->{$value});
+                        } else if (\gettype($this->{$value}) === 'object') {
+                            $data[$newKey] = new GenericResource($this->{$value});
+                        } else if (\gettype($this->{$value}) === 'string') {
+                            $data[$newKey] = $this->{$value};
                         }
                     } catch (\Throwable $th) {
-                       
-                    }                                       
+
+                    }
 
                 }
             }
         }
 
-        return $return ?? null;
+        return $data ?? null;
     }
 }
